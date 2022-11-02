993e2c9e68883fefe14ce7a2e8067e73f9877e76
Anna <anyaamdmitrieva@yandex.ru>
2022-11-02 23:37:25 +0600

    create method GetPointArea

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// вместо переменной в теле метода (намберАреа) можно использовать только возврат значения, либо через консоль вовращать сразу номер четверти,
+// но метод будет войд, а при вызове  вызывается просто метод, скрины есть в notion
+
+int GetPointArea(int coordX, int coordY)
+{
+    int numberArea = 0;
+    if (coordX > 0 && coordY > 0)
+    {
+        numberArea = 1;
+    }
+
+    if (coordX < 0 && coordY > 0)
+    {
+        numberArea = 2;
+    }
+
+    if (coordX < 0 && coordY < 0)
+    {
+        numberArea = 3;
+    }
+
+    if (coordX > 0 && coordY < 0)
+    {
+        numberArea = 4;
+    }
+    return numberArea;
+}
+
+Console.WriteLine("Введите число Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int numberArea = GetPointArea(x, y);
+Console.WriteLine(numberArea);
+
